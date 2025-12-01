661ff8b93dc10fbe03c2148a04d02dabdf2f0abd
Malakai <poncemalakai@gmail.com>
2025-11-30 21:46:08 -0800

    milestone 3

--- Lab_36.cpp
+++ Lab_36.cpp
@@ -4,32 +4,41 @@
 #include "IntBinaryTree.h"
 using namespace std;
 
+void pullFromFile(IntBinaryTree& tree, const string& filename) {
+    ifstream inFile("minicodes.txt");
+
+    if (!inFile) {
+        cout << "file not open" << endl;
+        return;
+    }
+
+    string code;
+    while (inFile >> code) {
+        tree.insertNode(code);
+    }
+}
+
 int main() {
     IntBinaryTree tree;
 
-    tree.insertNode("Apple");
-    tree.insertNode("Banana");
-    tree.insertNode("Cherry");
-    tree.insertNode("Lemon");
+    // tree.insertNode("Apple");
+    // tree.insertNode("Banana");
+    // tree.insertNode("Cherry");
+    // tree.insertNode("Lemon");
 
-    cout << "traversal: " << endl;
-    tree.displayInOrder();
+    // cout << "traversal: " << endl;
+    // tree.displayInOrder();
 
-    cout << "search for Banana: " 
-         << (tree.searchNode("Banana") ? "Found" : "Not Found") << endl;
+    // cout << "search for Banana: " 
+    //      << (tree.searchNode("Banana") ? "Found" : "Not Found") << endl;
 
     
     ifstream inFile("codes.txt");
 
-    if (!inFile) {
-        cout << "Unable to open file minicodes.txt";
-        return 1;
-    }   else {
-        cout << "file opened goooood" << endl;   
-    }
-
+    pullFromFile(tree, "codes.txt");
+    cout << "After pulling from file, traversal: " << endl;
+    tree.displayInOrder();
     return 0;
 
 }
 
-void pullFromFile()
